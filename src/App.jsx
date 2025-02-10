6d12663c74136b68a80f08e8b515252f36e51ab5
arturogutierrezdev <111131289+arturogutierrezdev@users.noreply.github.com>
2025-02-10 15:26:13 -0500

    Card Implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 
 import {useRef, useState} from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import './App.css'
+import Geometries from 'three/src/renderers/common/Geometries.js'
 
 //directly controls box
-function Box(props) {
+function CardModel(props)
+{
 const ref = useRef()
 const [hovered, hover]= useState(false)
 const [clicked, click] = useState(false)
-useFrame((state, delta) => (ref.current.rotation.x +=delta))
+const { nodes, materials } = useGLTF('/card.glb')
+useFrame((state, delta) => (ref.current.rotation.y +=delta))
 return (
+ <group {...props} dispose={null}>
   <mesh
-    {...props}
+    {...props}    
     ref={ref}
     scale={clicked ? 1.5 : 1}
     onClick={(event) => click(!clicked)}
     onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-    onPointerOut={(event) => hover(false)}>
-    <boxGeometry args={[1, 1, 1]} />
-    <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    onPointerOut={(event) => hover(false)}
+    geometry={nodes.Cube.geometry}
+    position={[0, 0, 0]}
+    >
+    <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
   </mesh>
+  
+  </group>
 )
 }
 
@@ -29,8 +39,7 @@ export default function App() {
     <>
     <div className={"canvas-container"}>
       <Canvas>
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
+      <CardModel position={[0, -1, -1]} />
         <ambientLight intensity = {0.5} />
         <directionalLight color="white" position={[0,0,5]} />
         <OrbitControls />
